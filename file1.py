2800e805cd91822825ab3af736589bdff6db199f
fooddudes <78228290+shortpupper@users.noreply.github.com>
2021-11-20 02:01:30 +0000

    it works yeahhhhh hashlooping works yeah

--- file1.py
+++ file1.py
@@ -25,6 +25,27 @@ def looping2(x, y):
   if done == 1:
     return "\nlooped "+str(y)+" times"
 
+whereruningvar = 0
+
+def whererunning(y):
+  x = whereruningvar
+  if x == 1:
+    print('code: '+str(y))
+  elif x == 0:
+    pass
+  else:
+    print('ERROR code: 6 ')
+
+# wr is the shortened function of 'whererunning'
+def wr(y):
+  x = whereruningvar
+  if x == 1:
+    print('code: '+str(y))
+  elif x == 0:
+    pass
+  else:
+    print('ERROR code: 6 ')
+
 ns= "\n\n"
 
 #m = hashlib.sha256()
@@ -71,11 +92,14 @@ def hashlooping(x, y, z, a):
   show = a
   var6 = 1
   var5 = y-1 # if loop=30 then this should be 29
-
+  var7 = a
+  var8 = 2
+  wr(1)
   while i <= y:
     var4 = "\nGen "+str(i+1)+":\n"
     if i == y: #would tell how many times looped
       done += 1
+      wr(2)
       break
     if z==1 or z=="on" or z=="n":
       if hasprintedonce == False:
@@ -91,11 +115,28 @@ def hashlooping(x, y, z, a):
         elif show == 1:
           if var6 == var5:
             print(var4+var3)
+            wr(8)
           elif var6 < var5:
             var6 += 1
           else:
             print('ERROR code: 1 ')
           lasshash += var3
+        elif show >= 2:
+          wr(3)
+          if i == var7 and i <= var5 and i >=var8: # if 0..y == y-1
+          #if 6 >= 2 and 6 <= 29
+          #when i == 6 and i <= 29 and i >=2
+          #when i == var7 and i <= var5 and i >=var8
+            return var4+var3 #output: Gen y:....
+            wr(4)
+          elif var6 < var5: # is 0..y less then y-1
+            wr(5)
+            var6 += 1 # var6 = n -> var6 = n+1
+          else:
+            wr(6)
+            print('ERROR code: 5 ')#print('code: 1 ')
+          lasshash += var3
+          wr(7)
         else:
           print('ERROR code: 2')
       else:
@@ -120,7 +161,7 @@ hashing1 = "hello"
 hashing2 = hashed(hashing1)
 
 
-var1 = hashlooping(hashing2, 30, 1, 1)
+var1 = hashlooping(hashing2, 30, 1, 6)
 
 
 print(str(var1)+gs)
